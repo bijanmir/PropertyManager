a991bb0b60a324541d2cf4a91b8db8d46e04a105
xxbijanxx <bijanxx@gmail.com>
2021-09-07 18:11:10 -0700

    Updated Tenants Component, Tickets Component, Account Component, Added Logo

--- PropertyManager/obj/Debug/net5.0/RazorDeclaration/Shared/SurveyPrompt.razor.g.cs
+++ PropertyManager/obj/Debug/net5.0/RazorDeclaration/Shared/SurveyPrompt.razor.g.cs
@@ -13,77 +13,77 @@ namespace PropertyManager.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "/Users/bijan/RiderProjects/PropertyManager/PropertyManager/_Imports.razor"
+#line 1 "C:\Users\bibia\RiderProjects\PropertyManager\PropertyManager\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "/Users/bijan/RiderProjects/PropertyManager/PropertyManager/_Imports.razor"
+#line 2 "C:\Users\bibia\RiderProjects\PropertyManager\PropertyManager\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "/Users/bijan/RiderProjects/PropertyManager/PropertyManager/_Imports.razor"
+#line 3 "C:\Users\bibia\RiderProjects\PropertyManager\PropertyManager\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "/Users/bijan/RiderProjects/PropertyManager/PropertyManager/_Imports.razor"
+#line 4 "C:\Users\bibia\RiderProjects\PropertyManager\PropertyManager\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "/Users/bijan/RiderProjects/PropertyManager/PropertyManager/_Imports.razor"
+#line 5 "C:\Users\bibia\RiderProjects\PropertyManager\PropertyManager\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "/Users/bijan/RiderProjects/PropertyManager/PropertyManager/_Imports.razor"
+#line 6 "C:\Users\bibia\RiderProjects\PropertyManager\PropertyManager\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "/Users/bijan/RiderProjects/PropertyManager/PropertyManager/_Imports.razor"
+#line 7 "C:\Users\bibia\RiderProjects\PropertyManager\PropertyManager\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "/Users/bijan/RiderProjects/PropertyManager/PropertyManager/_Imports.razor"
+#line 8 "C:\Users\bibia\RiderProjects\PropertyManager\PropertyManager\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "/Users/bijan/RiderProjects/PropertyManager/PropertyManager/_Imports.razor"
+#line 9 "C:\Users\bibia\RiderProjects\PropertyManager\PropertyManager\_Imports.razor"
 using PropertyManager;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "/Users/bijan/RiderProjects/PropertyManager/PropertyManager/_Imports.razor"
+#line 10 "C:\Users\bibia\RiderProjects\PropertyManager\PropertyManager\_Imports.razor"
 using PropertyManager.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "/Users/bijan/RiderProjects/PropertyManager/PropertyManager/_Imports.razor"
+#line 11 "C:\Users\bibia\RiderProjects\PropertyManager\PropertyManager\_Imports.razor"
 using PropertyManager.Components;
 
 #line default
@@ -97,7 +97,7 @@ using PropertyManager.Components;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 12 "/Users/bijan/RiderProjects/PropertyManager/PropertyManager/Shared/SurveyPrompt.razor"
+#line 12 "C:\Users\bibia\RiderProjects\PropertyManager\PropertyManager\Shared\SurveyPrompt.razor"
        
     // Demonstrates how a parent component can supply parameters
     [Parameter]
