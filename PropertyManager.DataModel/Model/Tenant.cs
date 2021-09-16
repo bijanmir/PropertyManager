8f9e88d8ab787ebb9335d1cf5cfd7564d9d532aa
Stefano Bieler <stefanobieler@gmail.com>
2021-09-15 18:14:06 -0700

    Refactored Tenant class

--- PropertyManager.DataModel/Model/Tenant.cs
+++ PropertyManager.DataModel/Model/Tenant.cs
@@ -2,10 +2,10 @@
 {
     public class Tenant
     {
+        public int TenantId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string PhoneNumber { get; set; }
         public string Email { get; set; }
-        public string BirthDate { get; set; }
     }
 }
